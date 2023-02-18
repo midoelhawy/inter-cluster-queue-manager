e109f0921f127c28c040fe07848c95469aaecd60
Ahmed hekal <ahekal369@gmail.com>
2023-02-18 14:24:19 +0100

    chore : disabile fastify logger and implment publisher endpoint

--- src/server.ts
+++ src/server.ts
@@ -4,42 +4,77 @@ import { sseClientManager } from "./clientsManager"
 const port = 3600
 
 
-const app = fastify({ logger: true })
+const app = fastify({ logger: false })
 
 
 app.get('/', async (request, res) => {
     return "inter-cluster-queue-manager-server up!"
+});
+
+
+
+
+app.post('/sendMessageToTopic', async (request, res) => {
+
+    const body = request.body as {
+        topic: string,
+        message_body: any
+    }
+
+    sseClientManager.sendEvents(body.topic, {
+        timestamp: new Date().toISOString(),
+        event: body.message_body,
+        topic:body.topic
+    },
+        [body.topic]
+    )
+
+    return {
+        success: true,
+        msg: `event successfully sent`
+    }
 })
 
 
 
 setInterval(() => {
     console.log(`${sseClientManager.connectedClientsNumber()} Consumer/s connected like now; curentActiveTopics=${sseClientManager.getAllActiveTopics().join()}`);
-}, 1000);
+}, 10 * 1000);
 
-async function sendRandomToTopic():Promise<void> {
-    console.log(`Trying to send random event to random topic`)
-    await new Promise((resolve) => {
-        setTimeout(resolve, +(Math.random() * 5000).toFixed())
-    })
-    const Topics = sseClientManager.getAllActiveTopics()
 
-    if (!Topics.length) return sendRandomToTopic()
-    const randomTopic = Topics[Math.floor(Math.random() * (Topics.length + 1))]
-    sseClientManager.sendEvents(randomTopic, {
-        time: new Date().toISOString(),
-        msg: "Its ok!",
-    },
-        [randomTopic]
-    )
 
-    return sendRandomToTopic()
-
+/* 
+if (process.env.DEBUG_MODE == "yes") {
+    setInterval(() => {
+        console.log(`${sseClientManager.connectedClientsNumber()} Consumer/s connected like now; curentActiveTopics=${sseClientManager.getAllActiveTopics().join()}`);
+    }, 1000);
+    
+    async function sendRandomToTopic():Promise<void> {
+        console.log(`Trying to send random event to random topic`)
+        await new Promise((resolve) => {
+            setTimeout(resolve, +(Math.random() * 5000).toFixed())
+        })
+        const Topics = sseClientManager.getAllActiveTopics()
+    
+        if (!Topics.length) return sendRandomToTopic()
+        const randomTopic = Topics[Math.floor(Math.random() * (Topics.length + 1))]
+        sseClientManager.sendEvents(randomTopic, {
+            time: new Date().toISOString(),
+            msg: "Its ok!",
+        },
+            [randomTopic]
+        )
+    
+        return sendRandomToTopic()
+    
+    }
+    
+    
+    sendRandomToTopic()
+    
 }
 
-
-sendRandomToTopic()
-
+*/
 
 
 app.get('/consume', {
@@ -63,7 +98,7 @@ app.get('/consume', {
         })
     }
 
-    console.log(req)
+    //console.log(req)
     const headers = {
         'Content-Type': 'text/event-stream',
         Connection: 'keep-alive',
@@ -87,9 +122,6 @@ app.get('/consume', {
         console.log(`Connection closed by client `)
         sseClientManager.deleteClient(clientId)
     })
-
-
-
 
 })
 
