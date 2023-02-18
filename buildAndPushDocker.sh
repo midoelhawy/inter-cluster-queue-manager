54e9957d52e8395a30ad5d698a8a39ee7d6cd210
Ahmed hekal <ahekal369@gmail.com>
2023-02-18 19:08:15 +0100

    fix: ts dependencies with production

--- buildAndPushDocker.sh
+++ buildAndPushDocker.sh
@@ -1,6 +1,7 @@
 # ! /bin/bash
 
-tag=$(node -p -e "require('./package.json').version+"-"+(new Date()).toISOString().replace(/\D/g,'')")
+tag=$(node -p -e "require('./package.json').version")
 echo "It will be tagged: $tag"
-docker build . -t midoelhawy/inter-cluster-queue-manager:$tag
-docker push midoelhawy/inter-cluster-queue-manager:$tag
+docker build . -t midoelhawy/inter-cluster-queue-manager:$tag  -t midoelhawy/inter-cluster-queue-manager:latest
+docker push midoelhawy/inter-cluster-queue-manager:$tag 
+docker push midoelhawy/inter-cluster-queue-manager:latest
