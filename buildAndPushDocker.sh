04419fcafc7760a8b6f5d2992231d3b6142acd54
Ahmed Hekal <ahekal@movia.biz>
2024-09-20 10:19:01 +0200

    dev: improve docker builder script

--- buildAndPushDocker.sh
+++ buildAndPushDocker.sh
@@ -2,6 +2,6 @@
 
 tag=$(node -p -e "require('./package.json').version")
 echo "It will be tagged: $tag"
-docker build . -t midoelhawy/inter-cluster-queue-manager:$tag  -t midoelhawy/inter-cluster-queue-manager:latest
-docker push midoelhawy/inter-cluster-queue-manager:$tag 
-docker push midoelhawy/inter-cluster-queue-manager:latest
+sudo docker build . -t midoelhawy/inter-cluster-queue-manager:$tag  -t midoelhawy/inter-cluster-queue-manager:latest
+sudo docker push midoelhawy/inter-cluster-queue-manager:$tag 
+sudo docker push midoelhawy/inter-cluster-queue-manager:latest
